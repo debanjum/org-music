ee26a20abce744c68ab6fe240a9add3057298da1
debanjum <debanjum@gmail.com>
2018-10-24 03:00:45 -0700

    Remove mpsyt. Interact with mpv directly and with playerctl over mpris
    mpsyt is a non-essential complication and increases fragility,
    maintenance overhead of the system.
    
    currently depend on mpv, youtube-dl, mpv-mpris support, playerctl,
    playerctl.el. This setup provides better player controls, cleaner code
    and more robust behavior.
    
    mpsyt already uses most of the other dependencies so this actually
    lowers fragility of system while increasing control.

--- org-music.el
+++ org-music.el
@@ -1,7 +1,10 @@
 (require 'cl)
 (require 'shell)
 (require 'org)
+(require 'playerctl)
 
+;; Org Music Library Media Control
+;; -------------------------------
 (defun flatten (l)
   (cond ((null l) nil)
    ((atom l) (list l))
@@ -43,80 +46,62 @@
   (flatten queries))
 
 (defun search-song-at-point ()
+  "use value of QUERY property if it exists to search provider, else use song headings"
   (interactive)
   (cond ((org-entry-get nil "QUERY"))
         ((nth 4 (org-heading-components)))))
 
-(defun play-song-at-point ()
-  "Open song at point"
-  (let* ((song-name (format "%s" (nth 4 (org-heading-components))))
-         (query (search-song-at-point)))
-    (message "Streaming: %s" song-name)
-    (log-song-state "ENQUEUED")
-    (mpsyt-execute (format "\n/%s\nadd 1\nvp\nall\n" (flatten query)))))
+
+;; Music Player Controls
+;; ---------------------
+
+;; Control MPV on Linux via Shell
+;; ------------------------------
+(defun mpv-start ()
+  (shell-command
+   (format "mpv --idle --script ~/.config/mpv/mpris.so --input-ipc-server=/tmp/mpvsocket --ytdl-format=bestaudio &")))
+
+(defun mpv-running? ()
+  (member "mpv" (split-string (shell-command-to-string "playerctl -l"))))
+
+(defun mpv-enqueue (search-query)
+  "enqueue in mpv first youtube result based on search-query"
+  (when (not (mpv-running?))
+      (mpv-start))
+  (shell-command-to-string
+   (format
+    "echo '{ \"command\": [\"loadfile\", \"ytdl://ytsearch:\\\"%s\\\"\", \"append-play\"] }' | socat - /tmp/mpvsocket"
+    search-query)))
 
 (defun enqueue-song-at-point ()
-  "Open song at point"
+  "enqueue song at point"
   (let ((song-name (format "%s" (nth 4 (org-heading-components))))
         (query (search-song-at-point)))
+    (mpv-enqueue (flatten query))
     (message "Streaming: %s" song-name)
-    (log-song-state "ENQUEUED")
-    (mpsyt-execute (format "\n/%s\nadd 1\nvp\n<SPC>\n" (flatten query)))))
+    (log-song-state "ENQUEUED")))
 
-(defun start-mpsyt ()
-  (let ((proc (start-process "*mpsyt" "*mpsyt*" "mpsyt")))
-    (with-current-buffer (process-buffer proc)
-      (display-buffer (current-buffer))
-      (shell-mode)
-      (mpsyt-mode)
-      (set-process-sentinel proc #'(lambda (process event) (if (equal "killed" event)
-                                                               (kill-buffer "*mpsyt*"))))
-      (set-process-filter proc 'comint-output-filter))))
-
-(defun mpsyt-execute (command)
-  "Send command to mpsyt process"
-  (interactive)
-  (let* ((proc-name "*mpsyt")
-         (proc (cond ((get-process proc-name))
-                     ((start-mpsyt)))))
-    (process-send-string proc-name command)))
-
-(defun mpsyt-playing? ()
-  "returns t if playing anything currently else nil"
-  (if (get-process "*mpsyt")
-      (with-temp-buffer
-        (progn
-          (insert-buffer-substring "*mpsyt*")
-          (end-of-buffer)
-          (previous-line)
-          (let* ((sline (buffer-substring-no-properties (line-beginning-position) (line-end-position)))
-                 (result (equal 0 (length sline))))
-            (not result))))))
+(defun play-song-at-point ()
+  "open song at point"
+  (let ((song-name (format "%s" (nth 4 (org-heading-components))))
+        (query (search-song-at-point)))
+    (mpv-enqueue (flatten query))
+    (message "Streaming: %s" song-name)
+    (log-song-state "ENQUEUED")))
 
 (defun enqueue-list ()
   "enqueue songs in active-region/sparse-tree/buffer"
   (interactive)
-  (if (mpsyt-playing?)
-      (mpsyt-execute "q"))
-  (let ((command
-         (format "%s\nvp\nall\n"
-                 (seq-reduce
-                  #'(lambda (a b) (concatenate 'string a (format "/%s\nadd 1\n" b)))
-                  (get-org-headings) ""))))
-    (message "%s" command)
-    (mpsyt-execute command)))
+  (mapcar #'mpv-enqueue (get-org-headings)))
 
-(defun play-list-mpsyt ()
+(defun play-list ()
   "play songs in active-region/sparse-tree/buffer"
   (interactive)
-  (if (get-process "*mpsyt")
-      (delete-process "*mpsyt*"))
-  (mpsyt-execute
-   (format "\nvp\nrm 1-\n%svp\nshuffle all\n"
-           (seq-reduce
-            #'(lambda (a b) (concatenate 'string a (format "/%s\nadd 1\n" b)))
-            (get-org-headings) ""))))
+  (mapcar #'mpv-enqueue (get-org-headings)))
 
+
+;; Control Music Player on Android via Termux
+;; ------------------------------------------
 (defun play-agenda (search-string)
   "play org-agenda filtered playlist"
   ;; filter songs in music library using search terms
@@ -126,57 +111,6 @@
   ;; get song-name from org playlist's headings, format it to enqueue and play in mpsyt, trigger mpsyt
   (play-list-on-android)
   (kill-buffer "playlist.org"))
-
-(defun play-mpsyt-buffer-song-at-point ()
-  "play song at point in mpsyt process buffer"
-  (interactive)
-  (let ((proc-name "*mpsyt")
-        (sline (buffer-substring-no-properties (line-beginning-position) (line-end-position))))
-    (mpsyt-execute "\n")
-    (search-backward sline)
-    (setq pline (buffer-substring-no-properties (line-beginning-position) (line-end-position)))
-    (mpsyt-execute (format "%s-\n" (car (split-string pline))))))
-
-(defun open-mpsyt-buffer ()
-  (interactive)
-  (cond ((get-process "*mpsyt"))
-        ((start-mpsyt)))
-  (switch-to-buffer-other-window "*mpsyt*"))
-
-(defun clear-playlist ()
-  "clear songs in mpsyt playlist"
-  (interactive)
-  (if (mpsyt-playing?)
-      (delete-process "*mpsyt*"))
-  (let ((command (format "vp\nrm 1-\n")))
-    (mpsyt-execute command)))
-
-(defun search-youtube (search-term)
-  "search songs in youtube"
-  (interactive "sEnter search term: ")
-  (message "\n/%s\n" search-term)
-  (mpsyt-execute (format "\n/%s\n" search-term))
-  (switch-to-buffer "*mpsyt*"))
-
-(defun append-outline-to-song-entries ()
-  "add outline parents of all visible song headings to their org entries"
-  (interactive)
-  (while (not (org-entry-get nil "SEQ"))
-    (org-next-visible-heading 1)
-    (let ((outline (org-display-outline-path nil nil " " t)))
-      (if (equal "song" (org-entry-get nil "TYPE"))
-          (insert-outline-of-entry outline)))))
-
-(defun insert-outline-of-entry (outline)
-  "add outline parents of song heading at point to its org entries"
-  (progn
-    (org-next-visible-heading 1)
-    (insert "\n")
-    (previous-line)
-    (insert outline)
-    (beginning-of-line)
-    (org-cycle)
-    (org-previous-visible-heading 1)))
 
 (defun play-list-on-android ()
   (android-play-org-entries (get-org-headings)))
@@ -198,45 +132,48 @@
   "share constructed youtube-url via termux to play on android youtube player"
   (shell-command-to-string (format "termux-open-url \"https://youtube.com/watch?v=%s\"" song-id)))
 
-(define-minor-mode mpsyt-mode
-  "Interact with mpsyt through emacs"
-  :lighter " Mpsyt"
-  :keymap (let ((map (make-sparse-keymap)))
-	    (define-key map (kbd "C-c m s") 'search-youtube)
-	    (define-key map (kbd "<SPC>") '(lambda () "play/pause" (interactive) (mpsyt-execute (kbd "SPC"))))
-	    (define-key map (kbd "q") 'delete-window)
-	    (define-key map (kbd "<") '(lambda () "play next track in playlist" (interactive) (mpsyt-execute (kbd "<"))))
-	    (define-key map (kbd "o") 'play-mpsyt-buffer-song-at-point)
-	    (define-key map (kbd ">") '(lambda () "play previous track in playlist" (interactive) (mpsyt-execute (kbd ">"))))
-	    (define-key map (kbd "0") '(lambda () "increase volume" (interactive) (mpsyt-execute (kbd "0"))))
-	    (define-key map (kbd "9") '(lambda () "decrease volume" (interactive) (mpsyt-execute (kbd "9"))))
-	    map))
 
+;; Org Music Library Metadata Enhancement Methods
+;; ----------------------------------------------
+(defun append-outline-to-song-entries ()
+  "add outline parents of all visible song headings to their org entries"
+  (interactive)
+  (while (not (org-entry-get nil "SEQ"))
+    (org-next-visible-heading 1)
+    (let ((outline (org-display-outline-path nil nil " " t)))
+      (if (equal "song" (org-entry-get nil "TYPE"))
+          (insert-outline-of-entry outline)))))
+
+(defun insert-outline-of-entry (outline)
+  "add outline parents of song heading at point to its org entries"
+  (progn
+    (org-next-visible-heading 1)
+    (insert "\n")
+    (previous-line)
+    (insert outline)
+    (beginning-of-line)
+    (org-cycle)
+    (org-previous-visible-heading 1)))
+
+;; Configure Org-Music Mode
 (define-minor-mode org-music-mode
   "Play music from org"
   :lighter " Org-music"
   :keymap (let ((map (make-sparse-keymap)))
-	    (define-key map (kbd "C-c m m") 'open-mpsyt-buffer)
-	    (define-key map (kbd "C-c m o") 'play-list)
-	    (define-key map (kbd "C-c m e") 'enqueue-list)
-	    (define-key map (kbd "C-c m s") 'search-youtube)
-	    (define-key map (kbd "C-c m <SPC>") '(lambda () "play/pause" (interactive) (mpsyt-execute (kbd "SPC"))))
-	    (define-key map (kbd "C-c m p") '(lambda () "play previous track in playlist" (interactive) (mpsyt-execute (kbd "<"))))
-	    (define-key map (kbd "C-c m n") '(lambda () "play next track in playlist" (interactive) (mpsyt-execute (kbd ">"))))
-	    (define-key map (kbd "C-c m 0") '(lambda () "increase volume" (interactive) (mpsyt-execute (kbd "0"))))
-	    (define-key map (kbd "C-c m 9") '(lambda () "decrease volume" (interactive) (mpsyt-execute (kbd "9"))))
-	    map)
+            (define-key map (kbd "C-c m o") 'play-list)
+            (define-key map (kbd "C-c m e") 'enqueue-list)
+            (define-key map (kbd "C-c m <SPC>") '(lambda () "play/pause" (interactive) (playerctl-play-pause-song)))
+            (define-key map (kbd "C-c m p") '(lambda () "play previous track in playlist" (interactive) (playerctl-previous-song)))
+            (define-key map (kbd "C-c m n") '(lambda () "play next track in playlist" (interactive) (playerctl-next-song)))
+            map)
 
   ;; define music speed commands
   (setq org-speed-commands-music
-	'(("o" . (lambda () "play song at point" (play-song-at-point)))
-	  ("e" . (lambda () "enqueue song at point" (enqueue-song-at-point)))
-	  ("m" . (lambda () "open mpsyt buffer" (open-mpsyt-buffer)))
-	  ("s" . (lambda () "play/pause" (message "toggle play/pause") (mpsyt-execute (kbd "SPC"))))
-	  ("j" . (lambda () "play next track in playlist" (mpsyt-execute (kbd "<"))))
-	  ("k" . (lambda () "play previous track in playlist" (mpsyt-execute (kbd ">"))))
-	  ("h" . (lambda () "increase volume" (mpsyt-execute (kbd "0"))))
-	  ("l" . (lambda () "decrease volume" (mpsyt-execute (kbd "9"))))))
+        '(("o" . (lambda () "play song at point" (play-song-at-point)))
+          ("e" . (lambda () "enqueue song at point" (enqueue-song-at-point)))
+          ("s" . (lambda () "play/pause" (message "toggle play/pause") (playerctl-play-pause-song)))
+          ("j" . (lambda () "play next track in playlist" (playerctl-next-song)))
+          ("k" . (lambda () "play previous track in playlist" (playerctl-previous-song)))))
 
   (defun org-speed-music (keys)
     "Use speed commands if at cursor at beginning of an org-heading line"
