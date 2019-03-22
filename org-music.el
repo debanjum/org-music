b47ae324a7e3d2e6c8912c3df8e25565c724055f
debanjum <debanjum@gmail.com>
2019-03-22 15:57:51 -0700

    Jump to random song. Automatically play a random song

--- org-music.el
+++ org-music.el
@@ -90,6 +90,19 @@
     "echo '{ \"command\": [\"loadfile\", \"ytdl://ytsearch:\\\"%s\\\"\", \"append-play\"] }' | socat - /tmp/mpvsocket"
     search-query)))
 
+(defun jump-to-random-song ()
+  "jump to a random song in the music library"
+  (interactive)
+  (let ((org-randomnote-candidates '("~/Notes/Music.org")))
+    (org-randomnote)
+    (while (not (equal "song" (org-entry-get nil "TYPE")))
+      (org-randomnote))))
+
+(defun play-random-song ()
+  (interactive)
+  (jump-to-random-song)
+  (play-song-at-point))
+
 (defun mpv-play (search-query)
   "enqueue in mpv first youtube result based on search-query"
   (when (not (mpv-running?))
