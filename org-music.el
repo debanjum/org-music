91fa7da8224776247ed1383ab7208efabdd508a6
debanjum <debanjum@gmail.com>
2019-04-13 23:43:46 -0700

    more packaging naming fixes, missing doc string etc

--- org-music.el
+++ org-music.el
@@ -50,7 +50,8 @@
 
 ;; Org Music Library Media Control
 ;; -------------------------------
-(defcustom org-music-files '("~/Notes/Music.org"))
+(defcustom org-music-files '("~/Notes/Music.org")
+  "Music org file location.")
 
 (defcustom org-music-media-directory "~/Music/OrgMusic/"
   "Media cache location.
@@ -71,13 +72,13 @@ Read, write path on Linux. Write path on Android relative to Termux root")
 (defvar org-music-last-playlist-filter nil
   "Last org filter used to create playlist.")
 
-(defun flatten (l)
-  "Flatten recursive list L."
+(defun org-music--flatten (l)
+  "Org-Music--Flatten recursive list L."
   (cond ((null l) nil)
    ((atom l) (list l))
-   (t (cl-loop for a in l appending (flatten a)))))
+   (t (cl-loop for a in l appending (org-music--flatten a)))))
 
-(defun get-org-headings ()
+(defun org-music--get-org-headings ()
   "Extract song headings from active/narrowed/sparse-tree region of org buffer."
   (interactive)
   (if (use-region-p)
@@ -93,7 +94,7 @@ Read, write path on Linux. Write path on Android relative to Termux root")
         (widen)
         headings))
 
-(defun log-song-state (state)
+(defun org-music--log-song-state (state)
   "Add STATE: `org-current-effective-time` to song's LOGBOOK."
   (interactive)
   (let ((opos (point))
@@ -106,7 +107,7 @@ Read, write path on Linux. Write path on Android relative to Termux root")
     (org-indent-region (line-beginning-position) (line-end-position))
     (goto-char opos)))
 
-(defun get-search-query ()
+(defun org-music--get-search-query ()
   "Retrieve QUERY property values or headings of org entries in active/narrowed/sparse-tree region of org buffer."
   (if (use-region-p)
       (narrow-to-region (point) (mark)))
@@ -115,15 +116,15 @@ Read, write path on Linux. Write path on Android relative to Termux root")
           (lambda (hs) (when (equal "song" (org-element-property :TYPE hs))
                          (org-element-property :QUERY hs))))))
     (widen)
-    (flatten queries)))
+    (org-music--flatten queries)))
 
-(defun search-song-at-point ()
+(defun org-music--search-song-at-point ()
   "Retrieve QUERY property value or heading of song at point."
   (interactive)
   (cond ((org-entry-get nil "QUERY"))
         ((nth 4 (org-heading-components)))))
 
-(defun get-song-source ()
+(defun org-music--get-song-source ()
   "Retrieve data source from CATEGORY property value of song's org-entry if it exists. Defaults to Youtube."
   (interactive)
   (cond ((org-entry-get nil "CATEGORY"))
@@ -134,40 +135,40 @@ Read, write path on Linux. Write path on Android relative to Termux root")
 
 ;; Control MPV on Linux via Shell
 ;; ------------------------------
-(defun mpv-start ()
+(defun org-music--mpv-start ()
   "Start mpv player."
   (shell-command
    (format "mpv --idle --input-ipc-server=/tmp/mpvsocket --ytdl-format=bestaudio &")))
 
-(defun mpv-running-p ()
+(defun org-music--mpv-running-p ()
   "Check if mpv is running."
   (member "mpv" (split-string (shell-command-to-string "playerctl -l"))))
 
-(defun get-media-url (search-query source)
+(defun org-music--get-media-url (search-query source)
   "Retrieve media url from SOURCE based on SEARCH-QUERY."
   (interactive)
   (if (equal "nextcloud" source)
-      (get-nextcloud-url search-query)
-    (get-youtube-url search-query)))
+      (org-music--get-nextcloud-url search-query)
+    (org-music--get-youtube-url search-query)))
 
-(defun emms-enqueue (search-query &optional source)
+(defun org-music--emms-enqueue (search-query &optional source)
   "Enqueue media retrieved from SOURCE based on SEARCH-QUERY."
-  (emms-add-url (get-media-url search-query source)))
+  (emms-add-url (org-music--get-media-url search-query source)))
 
-(defun emms-play (search-query &optional source)
+(defun org-music--emms-play (search-query &optional source)
   "Play media retrieved from SOURCE based on SEARCH-QUERY."
-  (emms-play-url (get-media-url search-query source)))
+  (emms-play-url (org-music--get-media-url search-query source)))
 
-(defun mpv-enqueue (search-query)
+(defun org-music--mpv-enqueue (search-query)
   "Enqueue in mpv the top result for SEARCH-QUERY on Youtube."
-  (when (not (mpv-running-p))
-      (mpv-start))
+  (when (not (org-music--mpv-running-p))
+      (org-music--mpv-start))
   (shell-command-to-string
    (format
     "echo '{ \"command\": [\"loadfile\", \"ytdl://ytsearch:\\\"%s\\\"\", \"append-play\"] }' | socat - /tmp/mpvsocket"
     search-query)))
 
-(defun jump-to-random-song (&optional match)
+(defun org-music--jump-to-random-song (&optional match)
   "Jump to a random song satisfying 'MATCH' in the music library."
   (interactive)
   (let ((org-randomnote-candidates org-music-files)
@@ -175,140 +176,138 @@ Read, write path on Linux. Write path on Android relative to Termux root")
     (setq org-music-last-playlist-filter match)
     (org-randomnote song-match)))
 
-(defun play-random-song (&optional match)
+(defun org-music--play-random-song (&optional match)
   "Play random song satisfying 'MATCH' in the music library."
   (interactive)
-  (jump-to-random-song (or match org-music-last-playlist-filter nil))
-  (play-song-at-point)
+  (org-music--jump-to-random-song (or match org-music-last-playlist-filter nil))
+  (org-music--play-song-at-point)
   (bury-buffer))
 
-(defun play-random-songs (&optional match)
+(defun org-music--play-random-songs (&optional match)
   "Play random songs satisfying 'MATCH' in the music library."
   (interactive)
   (let ((playlist-filter (or match org-music-last-playlist-filter)))
-    (play-random-song playlist-filter)
-    (add-hook 'emms-player-finished-hook 'play-random-songs)
-    (add-hook 'emms-player-stopped-hook #'(lambda () (remove-hook 'emms-player-finished-hook 'play-random-songs)))))
+    (org-music--play-random-song playlist-filter)
+    (add-hook 'emms-player-finished-hook 'org-music--play-random-songs)
+    (add-hook 'emms-player-stopped-hook #'(lambda () (remove-hook 'emms-player-finished-hook 'org-music--play-random-songs)))))
 
-(defun play-highlighted (start end)
+(defun org-music--play-highlighted (start end)
   "Play highlighted text between START and END."
   (interactive "r")
   (if (use-region-p)
       (let ((regionp (buffer-substring-no-properties start end)))
-        (emms-play-url (get-media-url regionp "youtube")))))
+        (emms-play-url (org-music--get-media-url regionp "youtube")))))
 
-(defun enqueue-highlighted (start end)
+(defun org-music--enqueue-highlighted (start end)
   "Enqueue highlighted text between START and END."
   (interactive "r")
   (if (use-region-p)
       (let ((regionp (buffer-substring-no-properties start end)))
-        (emms-add-url (get-media-url regionp "youtube")))))
+        (emms-add-url (org-music--get-media-url regionp "youtube")))))
 
-(defun mpv-play (search-query)
+(defun org-music--mpv-play (search-query)
   "Enqueue in mpv the top result for SEARCH-QUERY on Youtube."
-  (when (not (mpv-running-p))
-      (mpv-start))
+  (when (not (org-music--mpv-running-p))
+      (org-music--mpv-start))
   (shell-command-to-string
    (format
     "echo '{ \"command\": [\"loadfile\", \"ytdl://ytsearch:\\\"%s\\\"\", \"append\"] }' | socat - /tmp/mpvsocket"
     search-query)))
 
-(defun enqueue-song-at-point ()
+(defun org-music--enqueue-song-at-point ()
   "Enqueue song at point."
   (let ((song-name (format "%s" (nth 4 (org-heading-components))))
-        (query (search-song-at-point))
-        (source (get-song-source)))
-    ;(emms-enqueue (flatten query) source)
-    (play-cached-song (car (flatten query)) source t)
+        (query (org-music--search-song-at-point))
+        (source (org-music--get-song-source)))
+    (org-music--play-cached-song (car (org-music--flatten query)) source t)
     (message "Streaming: %s" song-name)
-    (log-song-state "ENQUEUED")))
+    (org-music--log-song-state "ENQUEUED")))
 
-(defun play-song-at-point ()
+(defun org-music--play-song-at-point ()
   "Open song at point."
   (let ((song-name (format "%s" (nth 4 (org-heading-components))))
-        (query (search-song-at-point))
-        (source (get-song-source)))
-    ;(emms-play (flatten query) source)
-    (play-cached-song (car (flatten query)) source nil)
+        (query (org-music--search-song-at-point))
+        (source (org-music--get-song-source)))
+    (org-music--play-cached-song (car (org-music--flatten query)) source nil)
     (message "Streaming: %s" song-name)
-    (log-song-state "ENQUEUED")))
+    (org-music--log-song-state "ENQUEUED")))
 
-(defun enqueue-list (&optional songs-list)
+(defun org-music--enqueue-list (&optional songs-list)
   "Enqueue SONGS-LIST in active/narrowed/sparse-tree region of org buffer."
   (interactive)
-  (let ((songs (or songs-list (get-org-headings))))
+  (let ((songs (or songs-list (org-music--get-org-headings))))
     (mapcar #'(lambda (s)
-                (apply #'play-cached-song (append s (list t))))
+                (apply #'org-music--play-cached-song (append s (list t))))
             songs)))
 
-(defun play-list ()
+(defun org-music--play-list ()
   "Play songs in active/narrowed/sparse-tree region of org buffer."
   (interactive)
-  (let ((songs (get-org-headings)))
-    (apply #'play-cached-song (pop songs))
-    (enqueue-list songs)))
+  (let ((songs (org-music--get-org-headings)))
+    (apply #'org-music--play-cached-song (pop songs))
+    (org-music--enqueue-list songs)))
 
 ;; Control Music Player on Android via Termux
 ;; ------------------------------------------
-(defun play-agenda (search-string)
-  "Play `org-agenda' filtered playlist based on SEARCH-STRING."
+(defun org-music--play-agenda-on-android (search-string)
+  "Play `org-agenda' SEARCH-STRING filtered playlist on android."
   ;; filter songs in music library using search terms
   (execute-kbd-macro (kbd (format "C-c a p %s SPC +{:TYPE:\\s-+song}" (replace-regexp-in-string " " " SPC " search-string))))
   ;; write filtered playlist to org file and open
   (org-agenda-write "~/.playlist.org" t)
   ;; get song-name from org playlist's headings, format it to enqueue and play in mpsyt, trigger mpsyt
-  (play-list-on-android)
+  (org-music--play-list-on-android)
   (kill-buffer ".playlist.org"))
 
-(defun play-list-on-android ()
+(defun org-music--play-list-on-android ()
   "Create playlist from org song headings and share via Termux to android music player."
-  (create-m3u-playlist (get-org-headings))
-  (android-share-playlist))
+  (org-music--create-m3u-playlist (org-music--get-org-headings))
+  (org-music--android-share-playlist))
 
-(defun create-m3u-playlist (song-entries)
+(defun org-music--create-m3u-playlist (song-entries)
   "Create m3u playlist from SONG-ENTRIES."
-  (write-playlist-to-file
+  (org-music--write-playlist-to-file
    (mapconcat
     #'(lambda (song)
         (format "#EXTINF:,%s\n%s"
                 (car song)
-                (apply #'cache-song (append song (list "android")))))
+                (apply #'org-music--cache-song (append song (list "android")))))
     song-entries
     "\n")))
 
-(defun write-playlist-to-file (m3u-playlist)
+(defun org-music--write-playlist-to-file (m3u-playlist)
   "Write M3U-PLAYLIST to file."
   (let ((playlist-file (format "%s%s" org-music-media-directory "orgmusic.m3u")))
     (write-region (format "#EXTM3U\n%s" m3u-playlist) nil playlist-file)))
 
-(defun android-share-playlist ()
+(defun org-music--android-share-playlist ()
   "Share playlist via termux to an android music player."
   (let ((playlist-file (format "%s%s" org-music-android-media-directory "orgmusic.m3u")))
   (shell-command-to-string (format "termux-open %S" playlist-file))))
 
-(defun get-youtube-url (search-query)
+(defun org-music--get-youtube-url (search-query)
   "Retrieve URL of the top result for SEARCH-QUERY on Youtube."
-  (format "https://youtube.com/watch?v=%s" (get-youtube-id-of-song (list search-query))))
+  (format "https://youtube.com/watch?v=%s" (org-music--get-youtube-id-of-song (list search-query))))
 
-(defun get-youtube-id-of-song (song-entry)
+(defun org-music--get-youtube-id-of-song (song-entry)
   "Retrieve id of top result for SONG-ENTRY on Youtube."
   (replace-regexp-in-string
    "\n$" ""
    (shell-command-to-string
     (format "youtube-dl --get-id ytsearch:\"%s\"" (car song-entry)))))
 
-(defun android-share-youtube-song (song-id)
+(defun org-music--android-share-youtube-song (song-id)
   "Share constructed youtube-url based on SONG-ID via termux to play on android youtube player."
   (shell-command-to-string (format "termux-open-url \"https://youtube.com/watch?v=%s\"" song-id)))
 
-(defun get-nextcloud-url (song-entry)
+(defun org-music--get-nextcloud-url (song-entry)
   "Retrieve URL of top result on Nextcloud for SONG-ENTRY."
   (replace-regexp-in-string
    "\n$" ""
    (shell-command-to-string
     (format "%s \"get_url\" \"%s\"" org-music-next-cloud-script (car song-entry)))))
 
-(defun get-song (name file-location)
+(defun org-music--get-song (name file-location)
   "Download NAME to FILE-LOCATION from Youtube."
   (interactive)
   (let ((download-command
@@ -316,10 +315,10 @@ Read, write path on Linux. Write path on Android relative to Termux root")
     (message "%s" download-command)
     (shell-command-to-string download-command)))
 
-(defun play-cached-song (song-entry &optional source enqueue)
+(defun org-music--play-cached-song (song-entry &optional source enqueue)
   "Cache SONG-ENTRY from SOURCE. Enqueue song if ENQUEUE true else play."
   (interactive)
-  (let ((uri-location (cache-song song-entry source)))
+  (let ((uri-location (org-music--cache-song song-entry source)))
     (message "location: %s, source: %s" uri-location source)
     (if (equal source "nextcloud")
         (if enqueue
@@ -329,25 +328,25 @@ Read, write path on Linux. Write path on Android relative to Termux root")
           (emms-add-file uri-location)
       (emms-play-file uri-location)))))
 
-(defun cache-song (song-name &optional source os)
+(defun org-music--cache-song (song-name &optional source os)
   "If SONG-NAME not available in local, download from SOURCE. Return local song URI based on OS."
   (interactive)
   (let ((song-file-location
          (format "%s%s.%s" org-music-media-directory song-name org-music-cache-song-format)))
     (message "cache location: %s, source: %s" song-file-location source)
     (if (equal source "nextcloud")
-        (get-nextcloud-url (if (listp song-name) song-name (list song-name)))
+        (org-music--get-nextcloud-url (if (listp song-name) song-name (list song-name)))
        ;; if file doesn't exist, trim cache and download file
       (if (not (file-exists-p song-file-location))
           (progn
-            (trim-cache)
-            (get-song song-name song-file-location)))
+            (org-music--trim-cache)
+            (org-music--get-song song-name song-file-location)))
       ;; return song uri based on operating system
       (if (equal os "android")
           (format "%s%s.%s" org-music-android-media-directory song-name org-music-cache-song-format)
         song-file-location))))
 
-(defun trim-cache ()
+(defun org-music--trim-cache ()
   "Trim media cache if larger than cache-size."
   (interactive)
   (let* ((sorted-files
@@ -363,16 +362,16 @@ Read, write path on Linux. Write path on Android relative to Termux root")
 
 ;; Org Music Library Metadata Enhancement Methods
 ;; ----------------------------------------------
-(defun append-outline-to-song-entries ()
+(defun org-music--append-outline-to-song-entries ()
   "Add outline parents of all visible song headings to their org entries."
   (interactive)
   (while (not (org-entry-get nil "SEQ"))
     (org-next-visible-heading 1)
     (let ((outline (org-display-outline-path nil nil " " t)))
       (if (equal "song" (org-entry-get nil "TYPE"))
-          (insert-outline-of-entry outline)))))
+          (org-music--insert-outline-of-entry outline)))))
 
-(defun append-outline-to-song-query-property ()
+(defun org-music--append-outline-to-song-query-property ()
   "Add outline parents of all visible song headings to their query property."
   (interactive)
   (while (not (org-entry-get nil "SEQ"))
@@ -381,7 +380,7 @@ Read, write path on Linux. Write path on Android relative to Termux root")
       (if (equal "song" (org-entry-get nil "TYPE"))
           (org-entry-put nil "QUERY" outline)))))
 
-(defun remove-colon-from-org-headings ()
+(defun org-music--remove-colon-from-org-headings ()
   "Remove colon from non song org headings."
   (interactive)
   (while (or (not (org-entry-get nil "SEQ"))
@@ -394,7 +393,7 @@ Read, write path on Linux. Write path on Android relative to Termux root")
             (if (equal ":" (string (char-before)))
                 (delete-char -1)))))))
 
-(defun insert-outline-of-entry (outline)
+(defun org-music--insert-outline-of-entry (outline)
   "Add OUTLINE parents of song heading at point to its org entry."
   (progn
     (org-next-visible-heading 1)
@@ -411,30 +410,28 @@ Read, write path on Linux. Write path on Android relative to Termux root")
   "Play music from org"
   :lighter " Org-music"
   :keymap (let ((map (make-sparse-keymap)))
-            (define-key map (kbd "C-c m o") 'play-list)
-            (define-key map (kbd "C-c m e") 'enqueue-list)
+            (define-key map (kbd "C-c m o") 'org-music--play-list)
+            (define-key map (kbd "C-c m e") 'org-music--enqueue-list)
             (define-key map (kbd "C-c m <SPC>") '(lambda () "play/pause" (interactive) (emms-pause)))
             (define-key map (kbd "C-c m p") '(lambda () "play previous track in playlist" (interactive) (emms-previous)))
             (define-key map (kbd "C-c m n") '(lambda () "play next track in playlist" (interactive) (emms-next)))
-            (define-key map (kbd "C-c m r") '(lambda () "play previous track in playlist" (interactive) (play-random-song)))
-            (define-key map (kbd "C-c m R") '(lambda () "play next track in playlist" (interactive) (play-random-songs)))
+            (define-key map (kbd "C-c m r") '(lambda () "play previous track in playlist" (interactive) (org-music--play-random-song)))
+            (define-key map (kbd "C-c m R") '(lambda () "play next track in playlist" (interactive) (org-music--play-random-songs)))
             map)
 
   ;; define music speed commands
-  (let ((org-speed-commands-music
-         '(("o" . (lambda () "play song at point" (play-song-at-point)))
-           ("e" . (lambda () "enqueue song at point" (enqueue-song-at-point)))
-           ("s" . (lambda () "play/pause" (message "toggle play/pause") (emms-pause)))
-           ("d" . (lambda () "play next track in playlist" (emms-previous)))
-           ("f" . (lambda () "play previous track in playlist" (emms-next))))))
-        
-    (defun org-speed-music (keys)
-      "Use speed commands if at cursor at beginning of an org-heading line"
-      (when (and (bolp) (looking-at org-outline-regexp))
-        (cdr (assoc keys org-speed-commands-music))))
+  (defun org-music--speed (keys)
+    "Use speed commands if at cursor at beginning of an org-heading line"
+    (when (and (bolp) (looking-at org-outline-regexp))
+      (cdr (assoc keys
+                  '(("o" . (lambda () "play song at point" (org-music--play-song-at-point)))
+                    ("e" . (lambda () "enqueue song at point" (org-music--enqueue-song-at-point)))
+                    ("s" . (lambda () "play/pause" (message "toggle play/pause") (emms-pause)))
+                    ("d" . (lambda () "play next track in playlist" (emms-previous)))
+                    ("f" . (lambda () "play previous track in playlist" (emms-next))))))))
 
     ;; add to org-speed-command-hook
-    (add-hook 'org-speed-command-hook 'org-speed-music)))
+  (add-hook 'org-speed-command-hook 'org-music--speed))
 
 (provide 'org-music)
 
